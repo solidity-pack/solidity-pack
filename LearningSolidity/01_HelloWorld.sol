e1c3471925e6079d9fd74a57ce8b0348b1987b9f
Raka Widhi Antoro <rakawidhiantoro@gmail.com>
2019-07-12 18:05:32 +0700

    Update 01_HelloWorld.sol

--- LearningSolidity/01_HelloWorld.sol
+++ LearningSolidity/01_HelloWorld.sol
@@ -4,9 +4,9 @@ pragma solidity >=0.4.22 <0.6.0; //Use the recommended version is v0.5.1+commit.
  * The HelloWorld contract does this and that...
  */
 contract HelloWorld {
-	string variabel = "Hello World";
+	string variable = "Hello World";
 
 	function HelloWorld () public view returns (string memory) {
-		return variabel;
+		return variable;
 	}	
 }
